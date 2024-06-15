e333984ed4b7855c015cd724e22f8e32bd35d6ce
Nelson Reis <82620017+NelsonReisSousa@users.noreply.github.com>
2024-06-15 19:55:31 -0300

    projeto-API/2

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2483f68877eb023ee74da968f88ce76bb840c6a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45bac3aafe19b190f47dda89d43a0d3bd1d583e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
